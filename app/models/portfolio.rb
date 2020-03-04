a198848b9b59c8dfafd48ff77c90c09e70fa21e9
haihuanchen <haihuan.chen@hotmail.com>
2020-03-04 16:26:09 -0500

    user/portfolios/reviews setup

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,4 +3,18 @@ class Portfolio < ApplicationRecord
     has_many :reviews
     has_many :investments
     has_many :stocks, through: :investments
+
+    
+    def array_data 
+        data = []
+        stocks.each_with_index do |s,i|
+            chunk = []
+            chunk << s.stock_name
+            chunk << s.current_price
+            chunk << self.investments[i].purchase_price
+            chunk << self.investments[i].num_of_shares
+            data << chunk
+        end 
+        data
+    end 
 end
